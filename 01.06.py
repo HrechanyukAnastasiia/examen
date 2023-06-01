b3692d65b54d58e145115782754bbf447ff364d1
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-06-01 21:05:37 +0300

    13

---
+++ 01.06.py
@@ -0,0 +1,8 @@
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+    def make_sound(self):
+        print(f"{self.name} робить звук: {self.sound}")
+animal = Animal("Корова", "Мууууууу")
+animal.make_sound()
