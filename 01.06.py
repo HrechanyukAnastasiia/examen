fb541ec1261a36929577cb657f12f762942f4ac9
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-06-01 20:33:35 +0300

    7

---
+++ 01.06.py
@@ -0,0 +1,12 @@
+#7
+students = {}
+n = int(input("Введіть кількість студентів:"))
+for _ in range(n):
+    name = input("Введіть ім'я студента:")
+    grade = float(input("Введіть оцінку з математики:"))
+    students[name] = grade
+max_grade = max(students.values())
+top_student = [name for name, grade in students.items() if grade == max_grade]
+print("Студенти з найвищою оцінкою:")
+for student in top_student:
+    print(student)
